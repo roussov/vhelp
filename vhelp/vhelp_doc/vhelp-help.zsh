ff9d229a7dc553b6dec19247e9aad9403e44f586
Vincent Rousseau <12572924+roussov@users.noreply.github.com>
2023-06-10 20:58:22 +0200

    Delete vhelp directory

--- vhelp/vhelp_doc/vhelp-help.zsh
+++
@@ -1,9 +0,0 @@
-##############################
-###Vhelp by vincentrs
-###Vhelp alias commands
-###version test
-alias vhelp -h = "sh /usr/src/vhelp/vhelp_doc/vhelp-help.sh"
-alias vhelp -maj = "sh /usr/src/vhelp/default-script/vhelp_majsys.sh"
-alias vhelp -@ = "firefox vincentrs92.github.io/vhelp-doc/"
-###@vincentrs - Vhelp Project
-###############################
