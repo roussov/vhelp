5e34c855fcf991a46e01af5b8c93133ed4560f20
Vincent Rousseau <12572924+roussov@users.noreply.github.com>
2023-06-14 02:04:51 +0200

    Create maj-commandes.sh

---
+++ vhelp_scripts/maj-commandes.sh
@@ -0,0 +1,19 @@
+# Pour mettre à jour les paquets de votre système, utilisez la commande suivante :
+
+sudo apt-get update
+
+# Pour installer les mises à jour disponibles, utilisez la commande suivante :
+
+sudo apt-get upgrade
+
+# Pour installer de nouveaux paquets qui n'ont pas été installés auparavant, utilisez la commande suivante :
+
+sudo apt-get install <nom_du_paquet>
+
+# Si vous utilisez une distribution Linux basée sur Debian ou Ubuntu, vous pouvez également utiliser la commande suivante pour mettre à jour l'ensemble du système :
+
+sudo apt-get dist-upgrade
+
+# Pour mettre à jour le noyau de votre système (systèmes basés sur Debian ou Ubuntu), utilisez la commande suivante :
+
+sudo apt-get install linux-image-generic
